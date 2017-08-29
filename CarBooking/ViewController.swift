198873401b84d88c8f66f77f2b2985a353a82ae4
rolfcasula <rolf.casula@gmail.com>
2017-08-29 10:43:38 +0200

    Initial Commit

---
+++ CarBooking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarBooking
+//
+//  Created by Rolf Alexandro Casula on Aug/29/17.
+//  Copyright © 2017 Rolf Alexandro Casula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
